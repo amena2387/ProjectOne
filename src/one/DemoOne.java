89496bd24188e2a1ff9defd570599269a7dbcb57
ayat <ayat@DKTP-02>
2021-11-24 21:02:45 -0400

    Added Randon Change PrintOut
    Added Void method

--- src/one/DemoOne.java
+++ src/one/DemoOne.java
@@ -4,7 +4,16 @@ public class DemoOne {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Anthony");
+		System.out.println("DemoOne");
+		
+		System.out.println("Randon Change...");
 	}
 
+	public static void add(int a, int b) {
+		
+		System.out.println(a+b);
+		
+		
+	}
+	
 }
